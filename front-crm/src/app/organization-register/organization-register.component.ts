371644db71f01922498530c3c4f41cf0e1ebb940
Arthur Gramm <arthurgramm@outlook.com.br>
2020-02-29 22:33:04 -0300

    redirect page ramo/erp

--- front-crm/src/app/organization-register/organization-register.component.ts
+++ front-crm/src/app/organization-register/organization-register.component.ts
@@ -13,8 +13,32 @@ export class OrganizationRegisterComponent implements OnInit {
   ngOnInit() {
   }
 
+  code: any;
+  generateCode() {
+    let randomString = function(lenght) {
+      let text = "";
+      let possible = "0123456789"
+
+      for(let i = 0; i < lenght; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+      return text;
+    }
+
+    this.code = randomString(8);
+    console.log(this.code);
+  }
+
   exit() {
     this.router.navigate([`/company/`]);
+  }
+
+  goToRamo() {
+    window.open('/ramo');
+  }
+
+  goToERP() {
+    window.open('/erp');
   }
 
 }
