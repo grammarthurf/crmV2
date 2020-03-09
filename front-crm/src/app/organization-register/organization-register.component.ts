59cc861bf43e1bef95346ee04e2d352b12f84b81
Arthur Gramm <arthurgramm@outlook.com.br>
2020-03-09 13:30:09 -0300

    buttons add company

--- front-crm/src/app/organization-register/organization-register.component.ts
+++ front-crm/src/app/organization-register/organization-register.component.ts
@@ -259,6 +259,33 @@ export class OrganizationRegisterComponent implements OnInit {
     }
   }
 
+  formatPhoneNumber(str) {
+    //Filter only numbers from the input
+    let cleaned = ('' + str).replace(/\D/g, '');
+
+    //Check if the input is of correct length
+    let match = cleaned.match(/^(\d{2})(\d{4})(\d{4})$/);
+  
+    if (match) {
+      return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+    };
+  
+    return null
+  };
+
+  formatCellPhoneNumber(str) {
+    //Filter only numbers from the input
+    let cleaned = ('' + str).replace(/\D/g, '');
+    
+    //Check if the input is of correct length
+    let match = cleaned.match(/^(\d{2})(\d{5})(\d{4})$/);
+  
+    if (match) {
+      return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+    };
+  
+    return null
+  };
 
 
   // addFields() {
