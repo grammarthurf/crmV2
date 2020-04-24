d8524e2bdaec8fc1dbd2602ab46f97907c635686
Arthur Gramm <arthurgramm@outlook.com.br>
2020-04-24 15:56:41 -0300

    layout leads

--- front-crm/src/app/funil/funil.component.ts
+++ front-crm/src/app/funil/funil.component.ts
@@ -10,62 +10,110 @@ export class FunilComponent implements OnInit {
 
   @Output() situationSidebar = new EventEmitter();
 
+  ticket: {
+    id: string;
+    // titulo: string;
+    empresa: string;
+    product: Array<object>[];
+    estagio: object;
+    valorestimado: number;
+    termometro: string;
+    vendedor: object;
+    status: string;
+    atividades: Array<object>[]
+  }
+
   tickets: any = [{
     id: 1,
-    titulo: 'ROSINA PORTAS - VEDOIS OEE',
+    // titulo: 'ROSINA PORTAS - VEDOIS OEE',
+    empresa: 'VEDOIS TECNOLOGIA',
+    product: [{ id: 1, title: 'Vedois OEE' }, { id: 2, title: 'Vedois CRM'}, { id: 3, title: 'Vedois Saúde'}, { id: 4, title: 'Vedois Planejamento'}],
     estagio: { id: 2 },
     valorestimado: 3500,
-    termometro: '0',
+    termometro: {value: '0', text: 'Regular'},
     vendedor: { id: 0, user: { username: 'Arthur' } },
     status: 'Ganhou',
     atividades: [{ id: 0 }, { id: 1 }]
 
   }, {
     id: 1,
-    titulo: 'PSR IMPRESSÃO VEDOIS OEE',
+    // titulo: 'ROSINA PORTAS - VEDOIS OEE',
+    empresa: 'VEDOIS TECNOLOGIA',
+    product: [{ id: 1, title: 'Vedois OEE' }, { id: 2, title: 'Vedois CRM'}, { id: 3, title: 'Vedois Saúde'}, { id: 4, title: 'Vedois Planejamento'}],
     estagio: { id: 2 },
     valorestimado: 3500,
-    termometro: '50',
+    termometro: {value: '50', text: 'Bom'},
     vendedor: { id: 0, user: { username: 'Arthur' } },
     status: 'Aberto',
     atividades: [{ id: 0 }, { id: 1 }]
 
   }, {
     id: 1,
-    titulo: 'ROHDEN - OEE',
+    // titulo: 'ROSINA PORTAS - VEDOIS OEE',
+    empresa: 'VEDOIS TECNOLOGIA',
+    product: [{ id: 1, title: 'Vedois OEE' }, { id: 2, title: 'Vedois CRM'}, { id: 3, title: 'Vedois Saúde'}, { id: 4, title: 'Vedois Planejamento'}],
+    estagio: { id: 2 },
+    valorestimado: 15000,
+    termometro: {value: '100', text: 'Ótimo'},
+    vendedor: { id: 0, user: { username: 'Arthur' } },
+    status: 'Aberto',
+    atividades: [{ id: 0 }, { id: 1 }]
+  }, {
+    id: 1,
+    // titulo: 'ROSINA PORTAS - VEDOIS OEE',
+    empresa: 'SULL AUTO',
+    product: [{ id: 1, title: 'Vedois OEE' }, { id: 2, title: 'Vedois CRM'}, { id: 3, title: 'Vedois Saúde'}, { id: 4, title: 'Vedois Planejamento'}, { id: 5, title: 'Vedois Logística'}],
     estagio: { id: 5 },
     valorestimado: 3500,
-    termometro: '0',
+    termometro: {value: '0', text: 'Regular'},
     vendedor: { id: 0, user: { username: 'Arthur' } },
     status: 'Aberto',
     atividades: [{ id: 0 }, { id: 1 }]
 
   }, {
     id: 1,
-    titulo: 'ALL4LABELS - MANUTENÇÃO',
+    // titulo: 'ROSINA PORTAS - VEDOIS OEE',
+    empresa: 'VEDOIS TECNOLOGIA',
+    product: [{ id: 1, title: 'Vedois OEE' }, { id: 2, title: 'Vedois CRM'}, { id: 3, title: 'Vedois Saúde'}, { id: 4, title: 'Vedois Planejamento'}],
     estagio: { id: 4 },
     valorestimado: 10000,
-    termometro: '0',
+    termometro: {value: '0', text: 'Regular'},
     vendedor: { id: 0, user: { username: 'Arthur' } },
     status: 'Aberto',
     atividades: [{ id: 0 }, { id: 1 }]
 
   }, {
     id: 1,
-    titulo: 'ARTELASSE - OEE MANUAL',
+    // titulo: 'ROSINA PORTAS - VEDOIS OEE',
+    empresa: 'VEDOIS TECNOLOGIA',
+    product: [{ id: 1, title: 'Vedois OEE' }, { id: 2, title: 'Vedois CRM'}, { id: 3, title: 'Vedois Saúde'}, { id: 4, title: 'Vedois Planejamento'}],
     estagio: { id: 3 },
     valorestimado: 0,
-    termometro: '50',
+    termometro: {value: '50', text: 'Bom'},
     vendedor: { id: 0, user: { username: 'Arthur' } },
     status: 'Aberto',
     atividades: [{ id: 0 }, { id: 1 }]
 
   }, {
     id: 1,
-    titulo: 'WEG-CESTARI - DDP',
+    // titulo: 'ROSINA PORTAS - VEDOIS OEE',
+    empresa: 'SULL AUTOMAÇÃO',
+    product: [{ id: 1, title: 'Vedois OEE' }, { id: 2, title: 'Vedois CRM'}, { id: 3, title: 'Vedois Saúde'}, { id: 4, title: 'Vedois Planejamento'}],
     estagio: { id: 5 },
     valorestimado: 3500,
-    termometro: '50',
+    termometro: {value: '50', text: 'Bom'},
+    vendedor: { id: 0, user: { username: 'Arthur' } },
+    status: 'Perdido',
+    atividades: [{ id: 0 }, { id: 1 }]
+
+  },{
+    id: 1,
+    // titulo: 'ROSINA PORTAS - VEDOIS OEE',
+    empresa: 'VEDOIS TECNOLOGIA',
+    product: [{ id: 1, title: 'Vedois OEE' }, { id: 2, title: 'Vedois CRM'}, { id: 3, title: 'Vedois Saúde'}, { id: 4, title: 'Vedois Planejamento'}],
+    estagio: { id: 7 },
+    // valorestimado: 50000,
+    termometro: {value: '50', text: 'Bom'},
     vendedor: { id: 0, user: { username: 'Arthur' } },
     status: 'Perdido',
     atividades: [{ id: 0 }, { id: 1 }]
@@ -127,14 +175,28 @@ export class FunilComponent implements OnInit {
       } else if (ticket.status === 'Perdido') {
         this.perdidas.tickets.push(ticket);
       }
+
+      // ticket.product.forEach(product => {
+      //   this.tickets.product = product.title.replace('Vedois', '');
+      //   console.log(this.tickets.product)
+      // })
+
+
     });
     console.log(this.perdidas.tickets);
+    console.log('CONTATADO:', this.contatado)
 
   }
 
+  cutVedois(word) {
+    const newWord = word.replace('Vedois', ' ');
+    return newWord;
+  }
 
-
-
+  redirectToAdd(url): void {
+    window.open(url);
+    window.focus();
+  }
 
   onClick() {
     this.showSidebar = !this.showSidebar;
--- front-crm/src/app/prospeccoes/prospeccoes.component.ts
+++ front-crm/src/app/prospeccoes/prospeccoes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-prospeccoes',
@@ -7,14 +7,124 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 })
 export class ProspeccoesComponent implements OnInit {
 
-  // @Input() showSidebar: boolean = false;
-  
-  // @Output() situationSidebar = new EventEmitter();
+  ticket: {
+    id: string;
+    // titulo: string;
+    empresa: string;
+    product: Array<object>[];
+    estagio: object;
+    valorestimado: number;
+    termometro: string;
+    vendedor: object;
+    status: string;
+    atividades: Array<object>[]
+  }
 
-  // onClick() {
-  //   this.showSidebar = !this.showSidebar;
-  //   this.situationSidebar.emit({ situation: this.showSidebar })
-  // }
+  tickets: any = [{
+    id: 1,
+    // titulo: 'ROSINA PORTAS - VEDOIS OEE',
+    empresa: 'VEDOIS TECNOLOGIA',
+    product: { id: 1, title: 'Vedois OEE' },
+    estagio: { id: 2 },
+    valorestimado: 3500,
+    termometro: '0',
+    vendedor: { id: 0, user: { username: 'Arthur' } },
+    status: 'Ganhou',
+    atividades: [{ id: 0 }, { id: 1 }]
+
+  }, {
+    id: 1,
+    // titulo: 'ROSINA PORTAS - VEDOIS OEE',
+    empresa: 'VEDOIS TECNOLOGIA',
+    product: { id: 1, title: 'Vedois OEE' },
+    estagio: { id: 2 },
+    valorestimado: 3500,
+    termometro: '50',
+    vendedor: { id: 0, user: { username: 'Arthur' } },
+    status: 'Aberto',
+    atividades: [{ id: 0 }, { id: 1 }]
+
+  }, {
+    id: 1,
+    // titulo: 'ROSINA PORTAS - VEDOIS OEE',
+    empresa: 'VEDOIS TECNOLOGIA',
+    product: { id: 1, title: 'Vedois OEE' },
+    estagio: { id: 2 },
+    valorestimado: 15000,
+    termometro: '100',
+    vendedor: { id: 0, user: { username: 'Arthur' } },
+    status: 'Aberto',
+    atividades: [{ id: 0 }, { id: 1 }]
+  }, {
+    id: 1,
+    // titulo: 'ROSINA PORTAS - VEDOIS OEE',
+    empresa: 'VEDOIS TECNOLOGIA',
+    product: { id: 1, title: 'Vedois OEE' },
+    estagio: { id: 5 },
+    valorestimado: 3500,
+    termometro: '0',
+    vendedor: { id: 0, user: { username: 'Arthur' } },
+    status: 'Aberto',
+    atividades: [{ id: 0 }, { id: 1 }]
+
+  }, {
+    id: 1,
+    // titulo: 'ROSINA PORTAS - VEDOIS OEE',
+    empresa: 'VEDOIS TECNOLOGIA',
+    // product: { id: 1, title: 'Vedois OEE' },
+    estagio: { id: 4 },
+    valorestimado: 10000,
+    termometro: '0',
+    vendedor: { id: 0, user: { username: 'Arthur' } },
+    status: 'Aberto',
+    atividades: [{ id: 0 }, { id: 1 }]
+
+  }, {
+    id: 1,
+    // titulo: 'ROSINA PORTAS - VEDOIS OEE',
+    empresa: 'VEDOIS TECNOLOGIA',
+    product: { id: 1, title: 'Vedois OEE' },
+    estagio: { id: 3 },
+    valorestimado: 0,
+    termometro: '50',
+    vendedor: { id: 0, user: { username: 'Arthur' } },
+    status: 'Aberto',
+    atividades: [{ id: 0 }, { id: 1 }]
+
+  }, {
+    id: 1,
+    // titulo: 'ROSINA PORTAS - VEDOIS OEE',
+    empresa: 'SULL AUTOMAÇÃO',
+    // product: { id: 1, title: 'Vedois OEE' },
+    estagio: { id: 5 },
+    valorestimado: 3500,
+    termometro: '50',
+    vendedor: { id: 0, user: { username: 'Arthur' } },
+    status: 'Perdido',
+    atividades: [{ id: 0 }, { id: 1 }]
+
+  },{
+    id: 1,
+    // titulo: 'ROSINA PORTAS - VEDOIS OEE',
+    empresa: 'VEDOIS TECNOLOGIA',
+    product: { id: 1, title: 'Vedois OEE' },
+    estagio: { id: 7 },
+    // valorestimado: 50000,
+    termometro: '50',
+    vendedor: { id: 0, user: { username: 'Arthur' } },
+    status: 'Perdido',
+    atividades: [{ id: 0 }, { id: 1 }]
+
+  },];
+
+  constructor() {
+
+  }
+
+  redirectToAdd(url): void {
+    window.open(url);
+    window.focus();
+  }
 
   ngOnInit() {
   }
